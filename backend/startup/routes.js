bf8b672c34a8090803143f4902ce78a8081be23a
bigjt_2 <bigjt_2@hotmail.com>
2023-10-31 14:55:24 -0400

    Removed CORS package, as it wasn't useful on AWS platform.

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const cors = require("cors");
 const pokedex = require("../routes/pokedex");
 
 module.exports = function (app) {
   app.use(express.urlencoded({ limit: "1mb", extended: true }));
   app.use(express.json({ limit: "1mb" }));
-  app.use(cors());
+  //Open CORS, trusting all origins, also had to add my custom apikey header
+  //(the CORS NPM library didn't work on AWS going between load balancers)
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Apikey"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    next();
+  });
   app.use(pokedex.POKEDEX_ENDPOINT, pokedex.Router);
 };
