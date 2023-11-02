092e2b6bad0fa73291e8b65db57967f4c98d538f
bigjt_2 <bigjt_2@hotmail.com>
2023-11-01 22:56:10 -0400

    Expanded healthcheck so it encompases database connection. Added curl to the container so the healthcheck could be more easily pinged from AWS

--- backend/routes/healthCheck.js
+++ backend/routes/healthCheck.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const winston = require("winston");
+const mongoose = require("mongoose");
 
 const HEALTHCHECK_ENDPOINT = "/api/healthcheck";
 
 router.get("/", async (req, res) => {
-  res.send("Pokedex running.");
+  dbState = mongoose.connection.readyState;
+
+  winston.info("Health check pinged.");
+  winston.info(`Database state: ${dbState}`);
+
+  if (dbState === 0 || dbState === 3) {
+    res.status(503).send("Pokedex connection to the database failed.");
+  } else {
+    res.status(200).send("Pokedex running.");
+  }
 });
 
 module.exports = { Router: router, HEALTHCHECK_ENDPOINT: HEALTHCHECK_ENDPOINT };
